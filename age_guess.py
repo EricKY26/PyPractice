b5531e63c3f1e2719af53aad97b5470504992085
Eric <ysj19921026@163.com>
2018-04-11 00:17:05 +0800

    add age_guess code

---
+++ age_guess.py
@@ -0,0 +1,7 @@
+real_age = 36
+age_guessed = int(input("Input the age you guessed: "))
+
+if age_guessed == real_age:
+    print("Congratulation! Your guess is right.")
+else:
+    print("Sorry, your guess is wrong.")
