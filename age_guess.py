4254a4a4aefb14d1e20b54b341e157db3d30886d
Eric <ysj19921026@163.com>
2018-04-11 16:14:49 +0800

    age guess multi choice

--- age_guess.py
+++ age_guess.py
@@ -3,5 +3,9 @@ age_guessed = int(input("Input the age you guessed: "))
 
 if age_guessed == real_age:
     print("Congratulation! Your guess is right.")
+elif age_guessed > real_age:
+    print("Sorry, you should try a smaller one.")
 else:
-    print("Sorry, your guess is wrong.")
+    print("Sorry, you should try a bigger one.")
+# else:
+#     print("Sorry, your guess is wrong.")
